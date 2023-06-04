f71e1150b5b466724952fa071f8066244a48829d
Shreyas Das <shreyas@MacShreyas.local>
2023-06-04 18:43:50 +0530

    all versions for singleton class

--- src/main/java/lld/design/singleton/ConnectionPool.java
+++ src/main/java/lld/design/singleton/ConnectionPool.java
@@ -1,5 +1,6 @@
 package lld.design.singleton;
 
+/*Version 1 */
 // eager initialization
 // public class ConnectionPool {
 
@@ -16,8 +17,34 @@ package lld.design.singleton;
 
 // }
 
+/* Version 2 */
 // On demand initialization
-public class ConnectionPool {
+// public class ConnectionPool {
+
+//     // Step 3: Add a static variable
+//     private static ConnectionPool instance = null;
+
+//     // Step 1: Hide constructor
+//     private ConnectionPool(){}
+
+//     // Step 2: Static Initializer
+//     public static ConnectionPool getInstance(){
+//         if (instance == null){
+//             instance = new ConnectionPool(); 
+//         }
+//         return instance;
+//     }
+
+// }
+
+/*
+ Problems:
+ If two threads try to operate at same time, then they will create two diff instances, so our solution is not thread safe
+ So, need to use lock using sync keyword
+ */
+
+ /* Version 3 */
+ public class ConnectionPool {
 
     // Step 3: Add a static variable
     private static ConnectionPool instance = null;
@@ -28,9 +55,12 @@ public class ConnectionPool {
     // Step 2: Static Initializer
     public static ConnectionPool getInstance(){
         if (instance == null){
-            instance = new ConnectionPool();
+            synchronized (ConnectionPool.class){
+                if (instance == null){
+                    instance = new ConnectionPool();
+                }
+            }
         }
         return instance;
     }
-
 }
