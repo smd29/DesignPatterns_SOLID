a47114a22cb65e614e77456d6fbd93943f8b2fab
Shreyas Das <shreyas@MacShreyas.local>
2023-06-04 20:53:13 +0530

    final builder

--- src/main/java/lld/design/builder/Student.java
+++ src/main/java/lld/design/builder/Student.java
@@ -1,20 +1,51 @@
 package lld.design.builder;
 import java.util.Date;
-import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.val;
 
+@Getter
 public class Student {
     private String name;
     private String email;
     private Integer age;
     private Date startDate;
 
-    // Step 1: create a constructor that accepts a hash map
-    public Student(StudentParams params){
-        this.name = params.getName();
-        this.email = params.getEmail();
-        this.age = params.getAge();
-        this.startDate = params.getStartDate();
+    // // Step 1: create a constructor that accepts a hash map
+    // public Student(StudentParams params){
+    //     this.name = params.getName();
+    //     this.email = params.getEmail();
+    //     this.age = params.getAge();
+    //     this.startDate = params.getStartDate();
+    // }
+
+
+    // Step 2: Hide outer constructor
+    private Student(){}
+    
+    // Improvement: Add a static initializer
+    public StudentBuilder toBuilder(){
+        return new StudentBuilder();
+    }
+    // Step 1: Create an inner class with same fields as outer class
+    @Getter
+    @Setter
+    public static class StudentBuilder{
+        private String name;
+        private String email;
+        private Integer age;
+        private Date startDate;
+
+        // Step 3: Implement build method
+        public Student build(){
+            Student student = new Student();
+            student.age = this.age;
+            student.email = this.email;
+            student.name = this.name;
+            student.startDate = this.startDate;
+
+            return student;
+        }
     }
 }
--- src/test/java/lld/design/builder/StudentTest.java
+++ src/test/java/lld/design/builder/StudentTest.java
@@ -4,15 +4,27 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import lld.design.builder.Student.StudentBuilder;
+
 public class StudentTest {
 
+    // @Test
+    // public void testBuilderInstance(){
+    //     StudentParams params = new StudentParams();
+    //     params.setAge(25);
+    //     params.setEmail("mariner@gmail.com");
+    //     params.setName("Mariner");
+
+    //     Student student = new Student(params);
+    // }
+    
     @Test
     public void testBuilderInstance(){
-        StudentParams params = new StudentParams();
-        params.setAge(25);
-        params.setEmail("mariner@gmail.com");
-        params.setName("Mariner");
+        StudentBuilder builder = new StudentBuilder();
+        builder.setAge(25);
+        builder.setEmail("mariner@gmail.com");
+        builder.setName("Mariner");
 
-        Student student = new Student(params);
+        Student student = builder.build();
     }
 }
