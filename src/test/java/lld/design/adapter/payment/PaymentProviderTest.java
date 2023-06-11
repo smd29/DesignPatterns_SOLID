bf6bba87d7cc3c384a91bd690d8f3baabfa26693
Shreyas Das <shreyas@MacShreyas.local>
2023-06-11 21:09:36 +0530

    added test case

--- src/test/java/lld/design/adapter/payment/PaymentProviderTest.java
+++ src/test/java/lld/design/adapter/payment/PaymentProviderTest.java
@@ -1,6 +1,9 @@
 package lld.design.adapter.payment;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import lld.design.adapter.payment.external.PaymentProvider;
 import lld.design.adapter.payment.external.RazorPayPaymentProvider;
@@ -11,5 +14,11 @@ public class PaymentProviderTest {
     @Before
     public void setUp(){
         provider = new RazorPayPaymentProvider();
+    }
+
+    @Test
+    public void testLink(){
+        String link = provider.link();
+        assertEquals("If link is called, Link should be returned", "Link", link);
     }
 }
